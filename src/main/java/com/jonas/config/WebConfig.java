52da8e95f61efc213f9bb29af50b2e7bd17519a8
shenjy <shenjiayun@hzyushi.cn>
2019-04-01 20:10:40 +0800

    添加消息转换器mediaType

--- src/main/java/com/jonas/config/WebConfig.java
+++ src/main/java/com/jonas/config/WebConfig.java
@@ -4,7 +4,11 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 参数解析
@@ -17,6 +21,10 @@ public class WebConfig extends WebMvcConfigurationSupport {
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        converter.setSupportedMediaTypes(mediaTypes);
+
         return new HttpMessageConverters(converter);
     }
 }
